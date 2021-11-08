5b5abe346031e5b9338616f1b484089bb6827eb1
Hiago Indalécio <118904@havan.com.br>
2021-11-08 14:48:51 -0300

    Accept and decline friend requests buttons action

--- web/src/components/FriendshipRequests/index.tsx
+++ web/src/components/FriendshipRequests/index.tsx
@@ -18,6 +18,8 @@ import './styles.css';
 
 import { BiCheckCircle, BiUserMinus } from "react-icons/bi";
 
+import ModalMessage from '../ModalMessages/ModalMessages';
+
 const StyledMenu = withStyles({
   paper: {
     border: '1px solid #d3d4d5',
@@ -42,7 +44,10 @@ export default function CustomizedNotificationMenus() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const { user } = useContext(AuthContext);
-  const { getFriendshipRequests } = useContext(FriendsContext);
+  const { getFriendshipRequests, declineFriendRequest, acceptFriendRequest } = useContext(FriendsContext);
+
+  const [isModalMessageVisible, setIsModalMessageVisible] = useState(false);
+  const [message, setMessage] = useState<string>('');
 
   let history = useHistory();
 
@@ -61,6 +66,18 @@ export default function CustomizedNotificationMenus() {
       history.push(`/profile/${cod}`);
     }
 
+    async function handleDeclineFriendRequest(codFriend: number) {
+      const result = await declineFriendRequest(user?.id as unknown as string, codFriend.toString());
+      setMessage(result.message);
+      setIsModalMessageVisible(true);
+    }
+  
+    async function handleAcceptFriendRequest(codFriend: number) {
+      const result = await acceptFriendRequest(user?.id as unknown as string, codFriend.toString());
+      setMessage(result.message);
+      setIsModalMessageVisible(true);
+    }
+
     async function getRequests() {
       const requests = await getFriendshipRequests(user?.id as unknown as string);
         if (requests.length > 0 && requests[0].cod_friend !== -1) {
@@ -73,15 +90,15 @@ export default function CustomizedNotificationMenus() {
 
             requests.forEach(request => {
               elements.push(
-                <MenuItem onClick={() => {goProfile(request.cod_friend)}}>
-                  <ListItemIcon>
+                <MenuItem className='header-menu-item' >
+                  <ListItemIcon onClick={() => {goProfile(request.cod_friend)}} className='cursor-pointer'>
                     <img src={request.pic_friend} className='profile-request-pic' alt="Profile" />
                   </ListItemIcon>
-                  <ListItemText primary={request.name_friend} secondary='Deseja ser seu amigo' />
-                  <div className='accept-friend'>
+                  <ListItemText primary={request.name_friend} secondary='Deseja ser seu amigo' onClick={() => {goProfile(request.cod_friend)}} className='cursor-pointer' />
+                  <div className='accept-friend cursor-pointer' onClick={() => {handleAcceptFriendRequest(request.cod_friend)}} >
                     <BiCheckCircle fontSize='extra-large'  />
                   </div>
-                  <div className="decline-friend">
+                  <div className="decline-friend cursor-pointer" onClick={() => {handleDeclineFriendRequest(request.cod_friend)}} >
                     <BiUserMinus fontSize='large' />
                   </div>
                 </MenuItem >
@@ -114,7 +131,7 @@ export default function CustomizedNotificationMenus() {
     return () => {
       clearInterval(interv);
     }
-  }, [getFriendshipRequests, user?.id, history, anchorEl])
+  }, [getFriendshipRequests, user?.id, history, anchorEl, acceptFriendRequest, declineFriendRequest])
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -132,6 +149,15 @@ export default function CustomizedNotificationMenus() {
         <PersonAddIcon fontSize='large' className='add-icon'/>
         <small className={`how-much ${howMuch > 0 ? '' : 'notVisible'}`} >{howMuch > 0 ? howMuch : ''}</small>
       </Button>
+      {
+        isModalMessageVisible ? 
+        <ModalMessage props={{message}} onClose={() => {
+            setIsModalMessageVisible(false);
+        }}>
+            
+        </ModalMessage> 
+        : null
+      }
       <StyledMenu
         id="customized-notifications-menu"
         anchorEl={anchorEl}
